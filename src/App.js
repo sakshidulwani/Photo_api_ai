a3807678cc02a6bd2a38177c7fde7e3f0c99285f
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-15 23:45:06 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,24 @@ function App() {
   const { image, setImage } = useState('');
 
   const handleChange = () => {
-    axios.get("https://api.generated.photos/api/v1/faces?api_key=iGKtGOOWlTm-DUpR2bXh_w")
+    axios
+      .get('https://api.generated.photos/api/v1/faces?api_key=iGKtGOOWlTm-DUpR2bXh_w')
+      .then(res => {
+        const uri = res.data.faces[0].urls[4][512]
+        uri && setImage(uri);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
   }
 
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1> AI Photo Generator</h1>
+      {image && <img src={image} alt="AI face" />}
+      <button type="button" onClick={handleChange}>
+        New Image
+      </button>
     </div>
   );
 }
